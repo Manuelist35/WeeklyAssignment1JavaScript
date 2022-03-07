6fbb4aa058258347bb1dc89dc8c8e51418197135
Manuelist35 <93733187+Manuelist35@users.noreply.github.com>
2022-03-07 09:25:52 -0800

    second commit

--- WeeklyAssignmet1Prab/WeeklyAssignmentPrab1.js
+++ WeeklyAssignmet1Prab/WeeklyAssignmentPrab1.js
@@ -7,12 +7,21 @@ async function getRepos() {
     const result =  await response.json()
     console.log(result, response)
 
-    /*result.map(value =>{
+    result.map(value =>{
         const title = value.title;
         const avatarUrl = value.user.avatar_url;
+        const labelsName = value.labels;
+        //console.log(labelsName)
+        labelsName.map(value =>{
+            nameLabel = value.name
+            console.log(nameLabel)
+            const ul = document.getElementById("listRepoIssues");
+            ul.innerHTML += `<div class="card">${nameLabel}</div>`
+        })
+        const number = value.number;
         console.log(title)
         console.log(avatarUrl)
         const ul = document.getElementById("listRepoIssues");
-        ul.innerHTML += `<li><div class="name"><a> Name: ${title}</a></div><div><div class="status">${avatarUrl}<div></li>`
-    })*/
+        ul.innerHTML += `<div class="card"><li><img class="from-avatar avatar-user" src="${avatarUrl}" width="20" height="20" alt="@mrYbc"><span class="title"><a>${title}</a></span><span class="number">#${number}</span></li></div><div class="labels"></div>`
+    })
 }
