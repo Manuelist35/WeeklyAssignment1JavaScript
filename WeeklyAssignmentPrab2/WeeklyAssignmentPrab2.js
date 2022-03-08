451a481d6faf16ddf43ddcc539abc01f2bbc8f22
Manuelist35 <93733187+Manuelist35@users.noreply.github.com>
2022-03-08 10:45:59 -0800

    second commit

--- WeeklyAssignmentPrab2/WeeklyAssignmentPrab2.js
+++ WeeklyAssignmentPrab2/WeeklyAssignmentPrab2.js
@@ -3,18 +3,18 @@ event.preventDefault();
 
 //console.log('working');
 
-var email = document.getElementById('email').value;
+var email = document.getElementById('email').value; // here we are defining pur three variable that we are gonna use later to create an object 
 var username = document.getElementById('username').value;
 var pass = document.getElementById('password').value;
 
-var user = {
+var user = { // In this line we are defining our object "user" that is going to be composed by the email, username and password value 
     email: email,
     username: username,
     password: pass,
 };
 
-var json = JSON.stringify(user);
-localStorage.setItem(username, json);
+var json = JSON.stringify(user); //the JSON.stringify() method converts a JavaScript object or value to a JSON string wich we are gonna use later 
+localStorage.setItem(username, json); //here we are using the localStorage wich is a property that allows JavaScript sites and apps to save key-value pairs in a web browser with no expiration date and also we are using the setItem method to store our values given by the user in the input boxes of our UI
 console.log('user added')
 
 }
@@ -26,7 +26,7 @@ function loginFunc(e){
     var pass = document.getElementById('password').value;
     var result = document.getElementById('result');
 
-    var user = localStorage.getItem(username);
+    var user = localStorage.getItem(username); //the getItem() method of the Storage interface, when passed a key name, will return that key's value, or null if the key does not exist, in the given Storage object.
     var data = JSON.parse(user);
     console.log(data);
 }
